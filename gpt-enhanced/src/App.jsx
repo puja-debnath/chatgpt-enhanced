707429079886118df65245bfebc533221a416755
unknown <pujadebnath570@gmail.com>
2023-11-08 22:24:59 +0530

    created first folder

--- gpt-enhanced/src/App.jsx
+++ gpt-enhanced/src/App.jsx
@@ -1,35 +1,25 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   <div className=' flex bg-gray-800  absolute left-0 right-0 top-0 bottom-0'>
+    <aside className='w-1/6 bg-gray-900  text-white items-center text-center p-4'>
+      <div className='border-2 border-radius-1 border-solid p-3 text-left hover:bg-gray-800'>
+        <span className='pl-2 pr-4 transition-all duration-200 ease-in-out'>+</span>New chat 
+     </div>
+
+       </aside>
+    <section className=' left-0 right-0 top-0 bottom-0'>
+      <h3>jdfhgjkzxvxdc</h3>
+    </section>
+   </div>
+
+
+    )
 }
 
 export default App
