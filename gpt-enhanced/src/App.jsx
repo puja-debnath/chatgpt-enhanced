663c06031dbe0e31c04b1c721ac99bb3510c6472
unknown <pujadebnath570@gmail.com>
2023-11-09 20:27:21 +0530

    updated appjsx

--- gpt-enhanced/src/App.jsx
+++ gpt-enhanced/src/App.jsx
@@ -7,14 +7,23 @@ function App() {
 
   return (
    <div className=' flex bg-gray-800  absolute left-0 right-0 top-0 bottom-0'>
-    <aside className='w-1/6 bg-gray-900  text-white items-center text-center p-4'>
+    <aside className='w-1/6 bg-gray-900  text-white  text-center p-4'>
       <div className='border-2 border-radius-1 border-solid p-3 text-left hover:bg-gray-800'>
         <span className='pl-2 pr-4 transition-all duration-200 ease-in-out'>+</span>New chat 
      </div>
 
        </aside>
-    <section className=' left-0 right-0 top-0 bottom-0'>
-      <h3>jdfhgjkzxvxdc</h3>
+    <section className='  '>
+      <div>header</div>
+      <div>middle</div>
+      <div className=''>
+        <textarea className='bg-gray-500 border-radius-2 border-none absolute bottom-4 w-4/5 m-4 outline-none 
+        text-left 
+        shadow-inset-b shadow-xl' 
+        placeholder='input values'
+        />
+
+      </div>
     </section>
    </div>
 
